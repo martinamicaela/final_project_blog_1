d76dd15df97522dd4ee3da73889650d4192122d1
martinamicaela frutos <martinamicaela924@gmail.com>
2024-03-23 21:02:37 +0000

    collect static

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -8,16 +8,7 @@ const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
-/**
-* Initializes edit functionality for the provided edit buttons.
-* 
-* For each button in the `editButtons` collection:
-* - Retrieves the associated comment's ID upon click.
-* - Fetches the content of the corresponding comment.
-* - Populates the `commentText` input/textarea with the comment's content for editing.
-* - Updates the submit button's text to "Update".
-* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
-*/
+
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
     let reviewId = e.target.getAttribute("review_id");
@@ -35,13 +26,3 @@ for (let button of deleteButtons) {
   });
 }
 
-/**
-* Initializes deletion functionality for the provided delete buttons.
-* 
-* For each button in the `deleteButtons` collection:
-* - Retrieves the associated comment's ID upon click.
-* - Updates the `deleteConfirm` link's href to point to the 
-* deletion endpoint for the specific comment.
-* - Displays a confirmation modal (`deleteModal`) to prompt 
-* the user for confirmation before deletion.
-*/
--- staticfiles/js/reviews.js
+++ staticfiles/js/reviews.js
@@ -3,16 +3,12 @@ const reviewText = document.getElementById("id_body");
 const reviewForm = document.getElementById("reviewForm");
 const submitButton = document.getElementById("submitButton");
 
-/**
-* Initializes edit functionality for the provided edit buttons.
-* 
-* For each button in the `editButtons` collection:
-* - Retrieves the associated comment's ID upon click.
-* - Fetches the content of the corresponding comment.
-* - Populates the `commentText` input/textarea with the comment's content for editing.
-* - Updates the submit button's text to "Update".
-* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
-*/
+
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
     let reviewId = e.target.getAttribute("review_id");
@@ -22,3 +18,11 @@ for (let button of editButtons) {
     reviewForm.setAttribute("action", `edit_review/${reviewId}`);
   });
 }
+for (let button of deleteButtons) {
+  button.addEventListener("click", (e) => {
+    let reviewId = e.target.getAttribute("review_id");
+    deleteConfirm.href = `delete_review/${reviewId}`;
+    deleteModal.show();
+  });
+}
+
